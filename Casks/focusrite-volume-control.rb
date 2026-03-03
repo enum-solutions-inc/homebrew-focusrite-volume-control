15b127513b8bbd39640b29571ada42085af27402
Nick Morozov <neurodeep@gmail.com>
2026-03-03 14:20:58 -0800

    bump to v1.3.1: fix About window version

--- Casks/focusrite-volume-control.rb
+++ Casks/focusrite-volume-control.rb
@@ -1,6 +1,6 @@
 cask "focusrite-volume-control" do
-  version "1.3"
-  sha256 "2e2a06c92d7fde980fc07a94b755ee72306ba541b72574f54d3d74d364710f83"
+  version "1.3.1"
+  sha256 "050f9cb8f5115fa817fcd235e6d813657a1862471a3034f9e418bca80ed2bb57"
 
   url "https://github.com/enum-solutions-inc/focusrite-volume-control/releases/download/v#{version}/FocusriteVolumeControl.dmg"
   name "Focusrite Volume Control"
